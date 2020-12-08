cce9296f055f8b8ac686a15cbc41ea7176117839
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2020-12-09 00:49:19 +0100

    Day1

---
+++ Day 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static List<int> ReadFile(string file)
+        {
+            List<int> res = new List<int>();
+            string path = Directory.GetCurrentDirectory() + "\\" + file;
+            if(!File.Exists(file))
+            {
+                Console.WriteLine("Couldn't find the file.\n");
+                return res;
+            }
+            StreamReader stream = new StreamReader(path);
+            while (stream.Peek() >= 0)
+            {
+                res.Add(int.Parse(stream.ReadLine()));
+            }
+            return res;
+        }
+        static void Main()
+        {
+            List<int> data = ReadFile("data.txt");
+            bool break_flag = false;
+            foreach(int elem in data)
+            {
+                foreach(int comp in data)
+                {
+                    if(elem+comp==2020)
+                    {
+                        Console.WriteLine("Result: {0}", comp * elem);
+                        break_flag = true;
+                        break;
+                    }
+                    if (break_flag) break;
+                }
+            }
+        }
+    }
+}
